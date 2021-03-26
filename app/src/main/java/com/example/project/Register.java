c6a501a347968c5550c37e2383aea380132563b8
hrithikmordani <hrithik.mordani36@nmims.edu.in>
2021-03-26 09:20:12 +0530

    First update

--- app/src/main/java/com/example/project/Register.java
+++ app/src/main/java/com/example/project/Register.java
@@ -6,21 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class Register extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Button btn_next;
     EditText name,contact,email,password;
     EditText confirmpassword;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String text;
 
 
 
@@ -28,7 +40,13 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(Register.this,R.array.occupation, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         name = findViewById(R.id.edit_text_full_name);
         contact = findViewById(R.id.edit_text_contact_number);
         email = findViewById(R.id.edit_text_email_register);
@@ -40,6 +58,8 @@ public class Register extends AppCompatActivity {
             public void onClick(View v) {
                 String semail = email.getText().toString().trim();
                 String spassword = password.getText().toString().trim();
+                String sname = name.getText().toString();
+                String scontact  = contact.getText().toString();
                 String sconfirmpassword = confirmpassword.getText().toString().trim();
                 if (TextUtils.isEmpty(semail)){
                     email.setError("Email is required");
@@ -63,9 +83,44 @@ public class Register extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
-                            Toast.makeText(Register.this,"User Created",Toast.LENGTH_SHORT).show();
-                            Intent next = new Intent(Register.this,register_2.class);
-                            startActivity(next);
+                            if (text.equals("Patient")){
+                                Toast.makeText(Register.this,"User Created",Toast.LENGTH_SHORT).show();
+                                Map<String,Object> patient = new HashMap<>();
+                                patient.put("Email",semail);
+                                patient.put("Name",sname);
+                                patient.put("Contact",scontact);
+                                fStore.collection("patients").add(patient).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Log.d("TAG","Document snapshot added with ID:" +documentReference.getId());
+                                    }
+                                });
+                                Intent next = new Intent(Register.this,register_2.class);
+                                next.putExtra("name",sname);
+                                startActivity(next);
+
+                            }
+                            else if(text.equals("Doctor")){
+                                Toast.makeText(Register.this,"User Created",Toast.LENGTH_SHORT).show();
+                                Map<String,Object> doctor = new HashMap<>();
+                                doctor.put("Email",semail);
+                                doctor.put("Name",sname);
+                                doctor.put("Contact",scontact);
+                                fStore.collection("doctors").add(doctor).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Log.d("TAG","Document snapshot added with ID:" +documentReference.getId());
+                                    }
+                                });
+                                Intent gotohome = new Intent(Register.this,MainActivity.class);
+                                gotohome.putExtra("name",sname);
+                                startActivity(gotohome);
+
+
+                            }
+
+
+
                         }else{
 
                             Toast.makeText(Register.this,"Error !"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
@@ -78,5 +133,15 @@ public class Register extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        text = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
